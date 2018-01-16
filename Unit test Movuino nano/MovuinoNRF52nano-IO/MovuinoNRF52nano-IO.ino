f4b74450da128c08884d9a5dd5fdd4a4ca1d2351
kevinlhoste <kevin.lhoste@gmail.com>
2018-01-16 09:50:11 +0100

    added unit tests for movuino nrf52 nano

---
+++ Unit test Movuino nano/MovuinoNRF52nano-IO/MovuinoNRF52nano-IO.ino
@@ -0,0 +1,43 @@
+/*
+ * 0 or A0 -> P0.31 digital ok analog ok PWM ok
+ * 1 or A1 -> P0.30 digital ok analog ok PWM ok 
+ * 2 or A2 -> P0.29 battery monitor A2 OK
+ * 3 or A3 -> P0.28 digital ok analog ok PWM ok
+ * 4 or A4 -> P0.02 digital ok analog ok PWM ok 
+ * 5 or A5 -> P0.03 digital ok analog ok PWM ok
+ * 6 or A6 -> P0.04 digital ok analog ok PWM ok
+ * 7 or A7 -> P0.05 digital ok analog ok PWM ok
+ * 8 -> P0.23 digital ok PWM ok
+ * 9 -> P0.22 digital ok PWM ok
+ * 10 ->  P0.8 digital ok PWM ok
+ * 11 or RED_LED ->  P0.25 digital ok PWM ok
+ * 12 ->  P0.26 digital ok PWM ok
+ * 13 ->  P0.27  Vibration  PWM ok
+ * 
+ * 20 -> P0.24 INTerrupt MPU
+ * 21 -> P0.20 Blue led digital ok NO PWM -> TODO change those pins for pwm pins
+ * 22 -> P0.19 Green led digital ok NO PWM -> TODO change those pins for pwm pins 
+ * 
+ *
+ */
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(9, OUTPUT);
+  //pinMode(12, OUTPUT);
+  Serial.begin(115200);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level) 
+  //int val=analogRead(A6);
+  //digitalWrite(12, HIGH);
+  delay(200);                       // wait for a second
+  digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
+  //digitalWrite(12, LOW);
+  delay(200);                       // wait for a second
+  Serial.println("I am groot !");
+  //Serial.println(val);
+}
